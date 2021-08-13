e181ec95adff014ac95e0e0f17cc35218779cf58
TS BIT <tomassag@gmail.com>
2021-08-13 15:47:59 +0300

    Last changes 0812

---
+++ src/cdw.js
@@ -0,0 +1,51 @@
+/*
+function problem(x) {
+  if (x !== "") {
+    return ((x * 50) + 6);
+}
+else {
+  if (x === "") {
+    return "Error";
+}
+}
+}
+
+console.log(problem(""))
+*/
+/*
+function numberToPower(number, power){
+  let n = number;
+  let p = power;
+  let s;
+  if (p === 0) {
+    s = 1;
+  } 
+  if (p === 1) {
+    s = n;  
+  }
+  if (p > 1) {
+  for (let i = p; i > 1; (i - 1)) {
+    let s = n;
+    s *= n;
+  }
+}
+return s;
+}
+
+console.log(numberToPower(10, 4));
+*/
+
+
+
+/*
+function arrayPlusArray(arr1, arr2) {
+  for (let i = 0; i < arr1.length; ++i) {
+  let s1 += arr1[i];
+  }
+  for (let j = 0; j < arr2.length; ++j) {
+  let s2 += arr2[j];
+  }
+  
+  return s1 + s2
+}
+*/
